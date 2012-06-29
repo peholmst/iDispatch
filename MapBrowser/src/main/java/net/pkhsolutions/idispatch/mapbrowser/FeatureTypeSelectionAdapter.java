da1b0fd66228d279ed275d20ebfdbb275ae0011a
Petter Holmstrom <petter.holmstrom@abo.fi>
2012-06-29 16:47:32 +0300

    Added JFeatureTypeSelectionComponent

---
+++ MapBrowser/src/main/java/net/pkhsolutions/idispatch/mapbrowser/FeatureTypeSelectionAdapter.java
@@ -0,0 +1,19 @@
+package net.pkhsolutions.idispatch.mapbrowser;
+
+import org.opengis.feature.type.FeatureType;
+
+/**
+ * TODO Document me!
+ *
+ * @author peholmst
+ */
+public class FeatureTypeSelectionAdapter implements FeatureTypeSelectionListener {
+
+    @Override
+    public void featureSelected(JFeatureTypeSelectionComponent sender, FeatureType featureType) {
+    }
+
+    @Override
+    public void featureDeselected(JFeatureTypeSelectionComponent sender, FeatureType featureType) {
+    }
+}
