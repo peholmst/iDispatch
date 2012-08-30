f2acd8bc0e706e4ccf3cd2c115ce84c1ad5d3ea9
Petter Holmstrom <petter.holmstrom@abo.fi>
2012-08-30 12:25:45 +0300

    Added MapBrowser click listener and click event

---
+++ MapBrowserWeb/src/main/java/net/pkhsolutions/idispatch/mapbrowserweb/components/MapBrowserClickEvent.java
@@ -0,0 +1,46 @@
+/*
+ * TODO Decide which license to use!
+ */
+package net.pkhsolutions.idispatch.mapbrowserweb.components;
+
+import com.vaadin.ui.Component;
+import java.awt.Point;
+
+/**
+ * This event is fired when a {@link MapBrowserComponent} is clicked.
+ *
+ * @author Petter Holmström
+ * @since 1.0
+ */
+public class MapBrowserClickEvent extends Component.Event {
+
+    private final Point.Double coordinates;
+
+    /**
+     * Creates a new {@code MapBrowserClickEvent}.
+     *
+     * @param source the map browser that was clicked.
+     * @param coordinates the lat/long coordinates of the point where the map
+     * was clicked (must not be {@code null}).
+     */
+    protected MapBrowserClickEvent(MapBrowserComponent source, Point.Double coordinates) {
+        super(source);
+        if (coordinates == null) {
+            throw new IllegalArgumentException("Coordinates must not be null");
+        }
+        this.coordinates = coordinates;
+    }
+
+    /**
+     * Returns the lat/long coordinates of the point where the map was clicked
+     * (never {@code null}).
+     */
+    public Point.Double getCoordinates() {
+        return coordinates;
+    }
+
+    @Override
+    public MapBrowserComponent getComponent() {
+        return (MapBrowserComponent) super.getComponent();
+    }
+}
