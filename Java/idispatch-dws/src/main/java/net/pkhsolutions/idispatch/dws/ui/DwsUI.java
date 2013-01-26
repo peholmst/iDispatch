a81f6708daf0d67ef18cca8443c73b66237b1d96
Petter Holmström <petter@vaadin.com>
2013-01-26 10:08:35 +0200

    files for new Java app

---
+++ Java/idispatch-dws/src/main/java/net/pkhsolutions/idispatch/dws/ui/DwsUI.java
@@ -0,0 +1,17 @@
+package net.pkhsolutions.idispatch.dws.ui;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.UI;
+
+/**
+ *
+ * @author peholmst
+ */
+public class DwsUI extends UI {
+    
+    @Override
+    protected void init(VaadinRequest request) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
