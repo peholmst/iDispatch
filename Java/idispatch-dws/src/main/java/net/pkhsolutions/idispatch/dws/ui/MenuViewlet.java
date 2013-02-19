2328668d89d98e6ee3abfdec4b2c4c6d991ea3f5
Petter Holmstrom <petter.holmstrom@abo.fi>
2013-02-19 15:14:30 +0200

    MenuViewlet is now accepting menu items dynamically

--- Java/idispatch-dws/src/main/java/net/pkhsolutions/idispatch/dws/ui/MenuViewlet.java
+++ Java/idispatch-dws/src/main/java/net/pkhsolutions/idispatch/dws/ui/MenuViewlet.java
@@ -1,11 +1,57 @@
 package net.pkhsolutions.idispatch.dws.ui;
 
+import com.vaadin.event.LayoutEvents;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
 /**
- * Created with IntelliJ IDEA.
- * User: peholmst
- * Date: 2013-02-10
- * Time: 12:11
- * To change this template use File | Settings | File Templates.
+ * @author peholmst
  */
-public class MenuViewlet {
+public class MenuViewlet extends CustomComponent {
+
+    private VerticalLayout layout;
+
+    public static class MenuItemRegistrationEvent {
+
+        private final MenuViewlet menu;
+
+        public MenuItemRegistrationEvent(MenuViewlet menu) {
+            this.menu = menu;
+        }
+
+        public MenuViewlet getMenu() {
+            return menu;
+        }
+    }
+    @Inject
+    javax.enterprise.event.Event<MenuItemRegistrationEvent> menuItemRegistration;
+
+    public MenuViewlet() {
+        addStyleName("menu-viewlet");
+        layout = new VerticalLayout();
+        layout.setMargin(true);
+        setCompositionRoot(layout);
+    }
+
+    @PostConstruct
+    protected void init() {
+        menuItemRegistration.fire(new MenuItemRegistrationEvent(this));
+    }
+
+    public void addMenuItem(String caption, final String viewId) {
+        HorizontalLayout menuItem = new HorizontalLayout();
+        menuItem.addStyleName("menu-item");
+        menuItem.addComponent(new Label(caption));
+        menuItem.addLayoutClickListener(new LayoutEvents.LayoutClickListener() {
+            @Override
+            public void layoutClick(LayoutEvents.LayoutClickEvent layoutClickEvent) {
+                getUI().getNavigator().navigateTo(viewId);
+            }
+        });
+        this.layout.addComponent(menuItem);
+    }
 }
