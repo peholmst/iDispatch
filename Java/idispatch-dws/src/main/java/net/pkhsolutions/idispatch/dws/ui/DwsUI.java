fb3d0b1d816bba450746fb8bcc4613e67262abe8
Petter Holmstrom <petter.holmstrom@abo.fi>
2013-02-27 22:33:50 +0200

    WIP-commit of the UI

--- Java/idispatch-dws/src/main/java/net/pkhsolutions/idispatch/dws/ui/DwsUI.java
+++ Java/idispatch-dws/src/main/java/net/pkhsolutions/idispatch/dws/ui/DwsUI.java
@@ -1,17 +1,72 @@
 package net.pkhsolutions.idispatch.dws.ui;
 
+import com.github.peholmst.i18n4vaadin.I18N;
+import com.github.peholmst.i18n4vaadin.annotations.Locale;
+import com.github.peholmst.i18n4vaadin.cdi.annotations.I18nSupportedLocales;
+import com.github.wolfie.refresher.Refresher;
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Widgetset;
+import com.vaadin.cdi.CDIViewProvider;
+import com.vaadin.cdi.Root;
+import com.vaadin.cdi.VaadinUI;
+import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.*;
+import com.vaadin.ui.themes.Reindeer;
+
+import javax.inject.Inject;
 
 /**
  *
  * @author peholmst
  */
+@VaadinUI
+@Root
+@Theme("dws")
+@Widgetset("net.pkhsolutions.idispatch.dws.ui.DwsWidgetset")
 public class DwsUI extends UI {
-    
+
+    @Inject
+    private CDIViewProvider cdiViewProvider;
+    @Inject
+    private OpenTicketsViewlet openTicketsViewlet;
+    @Inject
+    private MenuViewlet menuViewlet;
+    @I18nSupportedLocales({@Locale(language = "sv"), @Locale(language = "fi")})
+    @Inject
+    private I18N i18n;
+    private Refresher refresher;
+
     @Override
     protected void init(VaadinRequest request) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        getPage().setTitle("iDispatch DWS");
+
+        HorizontalSplitPanel rootLayout = new HorizontalSplitPanel();
+        rootLayout.setSizeFull();
+        rootLayout.setSplitPosition(200, Unit.PIXELS);
+        setContent(rootLayout);
+
+        VerticalLayout sidebar = new VerticalLayout();
+        sidebar.setSizeFull();
+        sidebar.addComponent(menuViewlet);
+        sidebar.addComponent(openTicketsViewlet);
+        sidebar.setExpandRatio(openTicketsViewlet, 1);
+
+        rootLayout.setFirstComponent(sidebar);
+
+        Panel viewContainer = new Panel();
+        viewContainer.addStyleName(Reindeer.PANEL_LIGHT);
+        viewContainer.setSizeFull();
+
+        rootLayout.setSecondComponent(viewContainer);
+
+        Navigator navigator = new Navigator(this, viewContainer);
+        navigator.addProvider(cdiViewProvider);
+        navigator.setErrorView(new ErrorView());
+
+        refresher = new Refresher();
+        refresher.setRefreshInterval(5000);
+        refresher.addListener(openTicketsViewlet);
+        addExtension(refresher);
     }
-    
 }
--- Java/idispatch-dws/src/main/java/net/pkhsolutions/idispatch/dws/ui/ErrorView.java
+++ Java/idispatch-dws/src/main/java/net/pkhsolutions/idispatch/dws/ui/ErrorView.java
@@ -1,11 +1,29 @@
 package net.pkhsolutions.idispatch.dws.ui;
 
-/**
- * Created with IntelliJ IDEA.
- * User: peholmst
- * Date: 2013-01-30
- * Time: 23:36
- * To change this template use File | Settings | File Templates.
- */
-public class ErrorView {
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.Reindeer;
+
+public class ErrorView extends VerticalLayout implements View {
+
+    private Label detailMessage;
+
+    public ErrorView() {
+        setSpacing(true);
+        setMargin(true);
+
+        Label header = new Label("The view does not exist");
+        header.addStyleName(Reindeer.LABEL_H1);
+        addComponent(header);
+
+        detailMessage = new Label();
+        addComponent(detailMessage);
+    }
+
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent viewChangeEvent) {
+        detailMessage.setValue(String.format("You attempted to navigate to the view \"%s\". Unfortunately, this view does not exist.", viewChangeEvent.getViewName()));
+    }
 }
