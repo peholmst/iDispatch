52f585879a3c4cde72212c0b1f7e6d1c022773a1
Petter Holmstrom <petter.holmstrom@abo.fi>
2013-02-27 15:00:27 +0200

    *ResourceStatus is also initialized when a new Resource is saved.

--- Java/idispatch-ejb/src/main/java/net/pkhsolutions/idispatch/ejb/masterdata/ResourceEJB.java
+++ Java/idispatch-ejb/src/main/java/net/pkhsolutions/idispatch/ejb/masterdata/ResourceEJB.java
@@ -1,10 +1,17 @@
 package net.pkhsolutions.idispatch.ejb.masterdata;
 
+import java.util.Calendar;
 import java.util.List;
 import java.util.logging.Logger;
 import javax.annotation.security.PermitAll;
 import javax.ejb.Stateless;
+import net.pkhsolutions.idispatch.ejb.common.ConcurrentModificationException;
+import net.pkhsolutions.idispatch.ejb.common.SaveFailedException;
+import net.pkhsolutions.idispatch.ejb.common.ValidationFailedException;
+import net.pkhsolutions.idispatch.entity.ArchivedResourceStatus;
+import net.pkhsolutions.idispatch.entity.CurrentResourceStatus;
 import net.pkhsolutions.idispatch.entity.Resource;
+import net.pkhsolutions.idispatch.entity.ResourceState;
 
 /**
  * EJB for managing {@link Resource}s.
@@ -20,6 +27,23 @@ public class ResourceEJB extends Backend<Resource> {
     @Override
     protected Logger log() {
         return log;
+    }
+
+    @Override
+    public Resource save(Resource entity) throws ValidationFailedException, ConcurrentModificationException, SaveFailedException {
+        boolean initializeStatusRecord = !entity.isPersistent();
+        Resource resource = super.save(entity);
+        if (initializeStatusRecord) {
+            CurrentResourceStatus status = new CurrentResourceStatus.Builder()
+                    .withResource(resource)
+                    .withState(ResourceState.UNAVAILABLE)
+                    .withTimestamp(Calendar.getInstance())
+                    .build();
+            em().persist(status);
+            em().persist(new ArchivedResourceStatus.Builder(status).build());
+            em().flush();
+        }
+        return resource;
     }
 
     @Override
