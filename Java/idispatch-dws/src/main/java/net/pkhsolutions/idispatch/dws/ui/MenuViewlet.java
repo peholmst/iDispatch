88de0d2805f97232a2fb594881aa63de7433463d
Petter Holmstrom <petter.holmstrom@abo.fi>
2013-03-02 09:25:21 +0200

    Added support for menu icons and ordering

--- Java/idispatch-dws/src/main/java/net/pkhsolutions/idispatch/dws/ui/MenuViewlet.java
+++ Java/idispatch-dws/src/main/java/net/pkhsolutions/idispatch/dws/ui/MenuViewlet.java
@@ -1,10 +1,15 @@
 package net.pkhsolutions.idispatch.dws.ui;
 
 import com.vaadin.event.LayoutEvents;
+import com.vaadin.server.Resource;
 import com.vaadin.ui.CustomComponent;
-import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Image;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
@@ -14,6 +19,8 @@ import javax.inject.Inject;
 public class MenuViewlet extends CustomComponent {
 
     private VerticalLayout layout;
+    private boolean initializing = false;
+    private List<VerticalLayout> menuItems = new ArrayList<>();
 
     public static class MenuItemRegistrationEvent {
 
@@ -38,13 +45,29 @@ public class MenuViewlet extends CustomComponent {
 
     @PostConstruct
     protected void init() {
-        menuItemRegistration.fire(new MenuItemRegistrationEvent(this));
+        initializing = true;
+        try {
+            menuItemRegistration.fire(new MenuItemRegistrationEvent(this));
+        } finally {
+            sortAndAddItems();
+            initializing = false;
+        }
     }
 
-    public void addMenuItem(String caption, final String viewId) {
-        HorizontalLayout menuItem = new HorizontalLayout();
+    public void addMenuItem(final String caption, final String viewId) {
+        addMenuItem(caption, viewId, null, 0);
+    }
+
+    public void addMenuItem(final String caption, final String viewId, final Resource icon, final int order) {
+        if (!initializing) {
+            throw new IllegalStateException("Menu items can currently only be added when the menu is being initialized");
+        }
+        VerticalLayout menuItem = new VerticalLayout();
         menuItem.setWidth("100%");
         menuItem.addStyleName("menu-item");
+        if (icon != null) {
+            menuItem.addComponent(new Image(null, icon));
+        }
         menuItem.addComponent(new Label(caption));
         menuItem.addLayoutClickListener(new LayoutEvents.LayoutClickListener() {
             @Override
@@ -52,6 +75,19 @@ public class MenuViewlet extends CustomComponent {
                 getUI().getNavigator().navigateTo(viewId);
             }
         });
-        this.layout.addComponent(menuItem);
+        menuItem.setData(order);
+        menuItems.add(menuItem);
+    }
+
+    private void sortAndAddItems() {
+        Collections.sort(menuItems, new Comparator<VerticalLayout>() {
+            @Override
+            public int compare(VerticalLayout o1, VerticalLayout o2) {
+                return ((Integer) o1.getData()).compareTo((Integer) o2.getData());
+            }
+        });
+        for (VerticalLayout menuItem : menuItems) {
+            layout.addComponent(menuItem);
+        }
     }
 }
