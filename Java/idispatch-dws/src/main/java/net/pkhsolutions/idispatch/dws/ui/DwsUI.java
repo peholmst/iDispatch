6f93e9bfb1c978606422251b5e3193fc8ea1445d
Petter Holmstrom <petter.holmstrom@abo.fi>
2013-03-02 09:50:31 +0200

    Added header

--- Java/idispatch-dws/src/main/java/net/pkhsolutions/idispatch/dws/ui/DwsUI.java
+++ Java/idispatch-dws/src/main/java/net/pkhsolutions/idispatch/dws/ui/DwsUI.java
@@ -2,6 +2,7 @@ package net.pkhsolutions.idispatch.dws.ui;
 
 import com.github.peholmst.i18n4vaadin.I18N;
 import com.github.peholmst.i18n4vaadin.annotations.Locale;
+import com.github.peholmst.i18n4vaadin.annotations.Message;
 import com.github.peholmst.i18n4vaadin.cdi.annotations.I18nSupportedLocales;
 import com.github.wolfie.refresher.Refresher;
 import com.vaadin.annotations.Theme;
@@ -9,12 +10,14 @@ import com.vaadin.annotations.Widgetset;
 import com.vaadin.cdi.CDIViewProvider;
 import com.vaadin.cdi.Root;
 import com.vaadin.cdi.VaadinUI;
+import com.vaadin.cdi.component.JaasTools;
 import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.*;
 import com.vaadin.ui.themes.Reindeer;
 
 import javax.inject.Inject;
+import net.pkhsolutions.idispatch.ejb.common.Roles;
 
 /**
  *
@@ -36,37 +39,64 @@ public class DwsUI extends UI {
     @Inject
     private I18N i18n;
     private Refresher refresher;
+    @Inject
+    private DwsUIBundle bundle;
 
+    @Message(key = "loggedInAs", value = "Inloggad som {0}")
     @Override
     protected void init(VaadinRequest request) {
+        refresher = new Refresher();
+        refresher.setRefreshInterval(5000);
+        addExtension(refresher);
+
         getPage().setTitle("iDispatch DWS");
 
-        HorizontalSplitPanel rootLayout = new HorizontalSplitPanel();
-        rootLayout.setSizeFull();
-        rootLayout.setSplitPosition(200, Unit.PIXELS);
-        setContent(rootLayout);
+        VerticalLayout root = new VerticalLayout();
+        root.setSizeFull();
+        setContent(root);
+
+        HorizontalLayout header = new HorizontalLayout();
+        header.setWidth("100%");
+        header.addStyleName("header");
+        {
+            Label lbl = new Label("iDispatch DWS");
+            lbl.addStyleName("title");
+            header.addComponent(lbl);
+        }
+        {
+            Label lbl = new Label(bundle.loggedInAs(JaasTools.getPrincipalName()));
+            lbl.addStyleName("current-user");
+            header.addComponent(lbl);
+            lbl.setSizeUndefined();
+            header.setComponentAlignment(lbl, Alignment.MIDDLE_RIGHT);
+        }
+        root.addComponent(header);
+
+        HorizontalSplitPanel splitLayout = new HorizontalSplitPanel();
+        splitLayout.setSizeFull();
+        splitLayout.setSplitPosition(200, Unit.PIXELS);
+        root.addComponent(splitLayout);
+        root.setExpandRatio(splitLayout, 1);
 
         VerticalLayout sidebar = new VerticalLayout();
         sidebar.setSizeFull();
         sidebar.addComponent(menuViewlet);
-        sidebar.addComponent(openTicketsViewlet);
-        sidebar.setExpandRatio(openTicketsViewlet, 1);
+        if (JaasTools.isUserInRole(Roles.DISPATCHER)) {
+            sidebar.addComponent(openTicketsViewlet);
+            sidebar.setExpandRatio(openTicketsViewlet, 1);
+            refresher.addListener(openTicketsViewlet);
+        }
 
-        rootLayout.setFirstComponent(sidebar);
+        splitLayout.setFirstComponent(sidebar);
 
         Panel viewContainer = new Panel();
         viewContainer.addStyleName(Reindeer.PANEL_LIGHT);
         viewContainer.setSizeFull();
 
-        rootLayout.setSecondComponent(viewContainer);
+        splitLayout.setSecondComponent(viewContainer);
 
         Navigator navigator = new Navigator(this, viewContainer);
         navigator.addProvider(cdiViewProvider);
         navigator.setErrorView(new ErrorView());
-
-        refresher = new Refresher();
-        refresher.setRefreshInterval(5000);
-        refresher.addListener(openTicketsViewlet);
-        addExtension(refresher);
     }
 }
