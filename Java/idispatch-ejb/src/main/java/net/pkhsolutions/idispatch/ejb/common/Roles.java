c57b783f11b2a8cf7f980b6b03c854a321294290
Petter Holmstrom <petter.holmstrom@abo.fi>
2013-03-02 09:55:59 +0200

    Added Roles class

---
+++ Java/idispatch-ejb/src/main/java/net/pkhsolutions/idispatch/ejb/common/Roles.java
@@ -0,0 +1,13 @@
+package net.pkhsolutions.idispatch.ejb.common;
+
+/**
+ *
+ * @author peholmst
+ */
+public final class Roles {
+
+    private Roles() {
+    }
+    public static final String ADMIN = "ADMIN";
+    public static final String DISPATCHER = "DISPATCHER";
+}
