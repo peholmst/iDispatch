2ae068829ecccddf86ed71a77420d2981697736a
Petter Holmstrom <petter.holmstrom@abo.fi>
2013-03-04 15:39:38 +0200

    Added sorting of resources and an initializing constructor

--- Java/idispatch-runboard/src/main/java/net/pkhsolutions/idispatch/runboard/rest/Notification.java
+++ Java/idispatch-runboard/src/main/java/net/pkhsolutions/idispatch/runboard/rest/Notification.java
@@ -1,8 +1,10 @@
 package net.pkhsolutions.idispatch.runboard.rest;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.Collections;
+import java.util.Objects;
 import java.util.Set;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -14,6 +16,8 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Notification {
 
+    @XmlAttribute
+    private Long id;
     @XmlAttribute(name = "timestamp")
     private Calendar timestamp;
     @XmlElement(name = "resource")
@@ -40,12 +44,33 @@ public class Notification {
     protected Notification() {
     }
 
+    public Notification(Long id, Calendar timestamp, Set<String> resources, String municipalitySv, String municipalityFi, Long ticketId, String address, String ticketTypeCode, String ticketTypeDescriptionSv, String ticketTypeDescriptionFi, Urgency urgency, String description) {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.resources = resources;
+        this.municipalitySv = municipalitySv;
+        this.municipalityFi = municipalityFi;
+        this.ticketId = ticketId;
+        this.address = address;
+        this.ticketTypeCode = ticketTypeCode;
+        this.ticketTypeDescriptionSv = ticketTypeDescriptionSv;
+        this.ticketTypeDescriptionFi = ticketTypeDescriptionFi;
+        this.urgency = urgency;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
     public Calendar getTimestamp() {
         return timestamp;
     }
 
-    public Set<String> getResources() {
-        return resources;
+    public Collection<String> getResources() {
+        ArrayList<String> sortedResources = new ArrayList<>(resources);
+        Collections.sort(sortedResources);
+        return sortedResources;
     }
 
     public String getMunicipalitySv() {
@@ -94,5 +119,27 @@ public class Notification {
             }
         }
         return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Notification other = (Notification) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
     }
 }
