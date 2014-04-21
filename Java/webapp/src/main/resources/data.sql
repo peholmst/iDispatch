d38cba22d181cc44e167899b246609807a93d986
Petter Holmström <petter@vaadin.com>
2014-04-22 00:54:06 +0300

    Added master data

---
+++ Java/webapp/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into municipalities (version, name) values (1, 'Pargas');
+insert into municipalities (version, name) values (1, 'Kimitoön');
+
+insert into ticket_types (version, code, description) values (1, '103', 'Brandlarm');
+insert into ticket_types (version, code, description) values (1, '202', 'Trafikolycka: liten');
+insert into ticket_types (version, code, description) values (1, '203', 'Trafikolycka: medelstor');
+insert into ticket_types (version, code, description) values (1, '401', 'Byggnadsbrand: liten');
+insert into ticket_types (version, code, description) values (1, '402', 'Byggnadsbrand: medelstor');
+insert into ticket_types (version, code, description) values (1, '411', 'Fordonsbrand: liten');
+insert into ticket_types (version, code, description) values (1, '412', 'Fordonsbrand: medelstor');
+insert into ticket_types (version, code, description) values (1, '421', 'Terrängbrand: liten');
+insert into ticket_types (version, code, description) values (1, '422', 'Terrängbrand: medelstor');
+insert into ticket_types (version, code, description) values (1, '431', 'Annan brand: liten');
+insert into ticket_types (version, code, description) values (1, '432', 'Annan brand: medelstor');
