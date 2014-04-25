591db0fcd28bb4449202641672a78e282ce19306
Petter Holmström <petter@vaadin.com>
2014-04-26 00:47:17 +0300

    Added query methods

--- Java/webapp/src/main/java/net/pkhsolutions/idispatch/domain/resources/ResourceRepository.java
+++ Java/webapp/src/main/java/net/pkhsolutions/idispatch/domain/resources/ResourceRepository.java
@@ -2,8 +2,20 @@ package net.pkhsolutions.idispatch.domain.resources;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Repository of {@link net.pkhsolutions.idispatch.domain.resources.Resource}s.
  */
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
+
+    /**
+     * Finds and returns the resource with the specified call sign, or {@code null} if not found.
+     */
+    Resource findByCallSign(String callSign);
+
+    /**
+     * Finds and returns all active resources.
+     */
+    List<Resource> findByActiveTrue();
 }
