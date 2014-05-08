e7a71d509a07d17f0f1ed0d1f691992b448782d8
Petter Holmström <petter@vaadin.com>
2014-05-08 15:41:09 +0300

    AssignmentService and DispatchService refactored

---
+++ Java/webapp/src/main/java/net/pkhsolutions/idispatch/boundary/AssignmentServiceBean.java
@@ -0,0 +1,77 @@
+package net.pkhsolutions.idispatch.boundary;
+
+import net.pkhsolutions.idispatch.boundary.events.AssignmentClosed;
+import net.pkhsolutions.idispatch.boundary.events.AssignmentOpened;
+import net.pkhsolutions.idispatch.boundary.events.AssignmentUpdated;
+import net.pkhsolutions.idispatch.entity.Assignment;
+import net.pkhsolutions.idispatch.entity.repository.AssignmentRepository;
+import net.pkhsolutions.idispatch.utils.UpdateResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+class AssignmentServiceBean extends AbstractServiceBean implements AssignmentService {
+
+    @Autowired
+    AssignmentRepository assignmentRepository;
+
+    @Override
+    public Optional<Assignment> findAssignment(Long id) {
+        logger.debug("Looking up assignment with ID {}", id);
+        if (id == null) {
+            return Optional.empty();
+        }
+        final Assignment assignment = assignmentRepository.findOne(id);
+        return Optional.ofNullable(assignment);
+    }
+
+    @Override
+    public Long openAssignment() {
+        logger.debug("Opening new assignment");
+        final Assignment createdAssignment = getTxTemplate().execute(status -> assignmentRepository.saveAndFlush(new Assignment()));
+        getApplicationContext().publishEvent(new AssignmentOpened(this, createdAssignment));
+        logger.debug("Opened assignment with ID {}", createdAssignment.getId());
+        return createdAssignment.getId();
+    }
+
+    @Override
+    public UpdateResult<Assignment> updateAssignment(Assignment assignment) {
+        logger.debug("Updating assignment {}", assignment);
+        if (assignment.isClosed()) {
+            logger.debug("Assignment {} is closed, ignoring", assignment);
+            return new UpdateResult.NoChange<>(assignment);
+        }
+        try {
+            final Assignment updatedAssignment = getTxTemplate().execute(status -> assignmentRepository.saveAndFlush(assignment));
+            getApplicationContext().publishEvent(new AssignmentUpdated(this, updatedAssignment));
+            return new UpdateResult.Success<>(updatedAssignment);
+        } catch (OptimisticLockingFailureException ex) {
+            logger.debug("Assignment {} was updated by another user, cannot update", assignment);
+            final Assignment updatedAssignment = assignmentRepository.findOne(assignment.getId());
+            return new UpdateResult.Conflict<>(assignment, updatedAssignment);
+        }
+    }
+
+    @Override
+    public void closeAssignment(Assignment assignment) {
+        logger.debug("Closing assignment {}", assignment);
+        if (assignment.isClosed()) {
+            logger.debug("Assignment {} is already closed, ignoring", assignment);
+            return;
+        }
+        assignment.setClosed(new Date());
+        final Assignment closedAssignment = getTxTemplate().execute(status -> assignmentRepository.saveAndFlush(assignment));
+        getApplicationContext().publishEvent(new AssignmentClosed(this, closedAssignment));
+    }
+
+    @Override
+    public List<Assignment> findOpenAssignments() {
+        logger.debug("Looking up open assignments");
+        return assignmentRepository.findByClosedIsNull();
+    }
+}
