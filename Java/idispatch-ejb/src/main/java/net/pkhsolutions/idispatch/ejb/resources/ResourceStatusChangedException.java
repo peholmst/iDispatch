725abe2ac2644334f81b787e26bb930d5c555cbd
Petter Holmström <petter@vaadin.com>
2014-05-08 21:00:07 +0300

    Deleted old modules

--- Java/idispatch-ejb/src/main/java/net/pkhsolutions/idispatch/ejb/resources/ResourceStatusChangedException.java
+++
@@ -1,6 +0,0 @@
-package net.pkhsolutions.idispatch.ejb.resources;
-
-import net.pkhsolutions.idispatch.ejb.common.ConcurrentModificationException;
-
-public class ResourceStatusChangedException extends ConcurrentModificationException {
-}
--- Java/idispatch-entity/src/main/java/net/pkhsolutions/idispatch/entity/TicketUrgency.java
+++
@@ -1,5 +0,0 @@
-package net.pkhsolutions.idispatch.entity;
-
-public enum TicketUrgency {
-    A, B, C, D
-}
