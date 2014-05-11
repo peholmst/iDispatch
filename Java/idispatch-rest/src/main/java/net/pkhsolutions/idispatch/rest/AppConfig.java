aed03ebbf3c56bde25444e0b747cee64320bbba7
Petter Holmström <petter@vaadin.com>
2014-05-11 23:03:06 +0300

    Deleted old files

--- Java/idispatch-rest/src/main/java/net/pkhsolutions/idispatch/rest/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package net.pkhsolutions.idispatch.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class AppConfig extends Application {
-}
