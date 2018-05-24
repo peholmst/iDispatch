a5ed2f805bcd62b5084c31f20e4fef9800e27cc8
Petter Holmström <petter.holmstrom@outlook.com>
2018-05-25 00:20:17 +0300

    WIP

--- JavaModules/idispatch-web/src/main/java/net/pkhapps/idispatch/web/ui/WebApplication.java
+++ JavaModules/idispatch-web/src/main/java/net/pkhapps/idispatch/web/ui/WebApplication.java
@@ -1,10 +1,8 @@
 package net.pkhapps.idispatch.web.ui;
 
 import net.pkhapps.idispatch.application.ApplicationConfig;
-import net.pkhapps.idispatch.web.ui.dispatch.DispatchConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
---
+++ JavaModules/idispatch-web/src/test/java/net/pkhapps/idispatch/web/ui/common/event/UIEventBrokerTest.java
@@ -0,0 +1,8 @@
+package net.pkhapps.idispatch.web.ui.common.event;
+
+/**
+ * Unit test for {@link UIEventBroker}.
+ */
+public class UIEventBrokerTest {
+    // TODO Write unit test for UIEventBroker.
+}
