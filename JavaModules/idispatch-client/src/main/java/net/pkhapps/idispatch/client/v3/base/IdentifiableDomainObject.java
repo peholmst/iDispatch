9bce5a8f831d5b6a9b617466049a462bc42358b1
Petter Holmström <petter.holmstrom@outlook.com>
2018-09-22 23:23:07 +0300

    Add AuditedDomainObject Add Principal Add VersionedDomainObject Change IdentifiableDomainObject to work with any ID type

--- JavaModules/idispatch-client/src/main/java/net/pkhapps/idispatch/client/v3/base/IdentifiableDomainObject.java
+++ JavaModules/idispatch-client/src/main/java/net/pkhapps/idispatch/client/v3/base/IdentifiableDomainObject.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 /**
  * Interface implemented by domain objects that can be identified by some unique ID.
  */
-public interface IdentifiableDomainObject<ID extends DomainObjectId> extends Serializable {
+public interface IdentifiableDomainObject<ID extends Serializable> extends Serializable {
 
     @Nonnull
     ID id();
