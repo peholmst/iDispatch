cd13154d519a7f5c4db8a2af0b377ea6ec937f45
Petter Holmström <petter.holmstrom@outlook.com>
2018-09-23 13:34:51 +0300

    Add GsonConfigurer

---
+++ JavaModules/idispatch-client/src/main/java/net/pkhapps/idispatch/client/v3/gson/GsonConfigurer.java
@@ -0,0 +1,43 @@
+package net.pkhapps.idispatch.client.v3.gson;
+
+import com.google.gson.GsonBuilder;
+import net.pkhapps.idispatch.client.v3.AssignmentId;
+import net.pkhapps.idispatch.client.v3.base.gson.DomainObjectIdJsonTypeAdapter;
+import net.pkhapps.idispatch.client.v3.type.*;
+import net.pkhapps.idispatch.client.v3.util.Color;
+import net.pkhapps.idispatch.client.v3.util.MultilingualString;
+import net.pkhapps.idispatch.client.v3.util.PhoneNumber;
+import net.pkhapps.idispatch.client.v3.util.gson.ColorJsonTypeAdapter;
+import net.pkhapps.idispatch.client.v3.util.gson.InstantJsonTypeAdapter;
+import net.pkhapps.idispatch.client.v3.util.gson.MultilingualStringTypeAdapter;
+import net.pkhapps.idispatch.client.v3.util.gson.PhoneNumberJsonTypeAdapter;
+
+import javax.annotation.Nonnull;
+import java.time.Instant;
+import java.util.Objects;
+
+/**
+ * Utility class for configuring a {@link com.google.gson.Gson}-instance through its {@link GsonBuilder}. This
+ * configurer will register all the type adapters needed by the iDispatch Client module, among other things.
+ */
+public class GsonConfigurer {
+
+    @Nonnull
+    public GsonBuilder configure(@Nonnull GsonBuilder gsonBuilder) {
+        Objects.requireNonNull(gsonBuilder, "gsonBuilder must not be null");
+        return gsonBuilder
+                .registerTypeAdapter(AssignmentId.class, new DomainObjectIdJsonTypeAdapter<>(AssignmentId::new))
+                .registerTypeAdapter(AssignmentStateId.class, new DomainObjectIdJsonTypeAdapter<>(AssignmentStateId::new))
+                .registerTypeAdapter(AssignmentTypeId.class, new DomainObjectIdJsonTypeAdapter<>(AssignmentTypeId::new))
+                .registerTypeAdapter(Color.class, new ColorJsonTypeAdapter())
+                .registerTypeAdapter(Instant.class, new InstantJsonTypeAdapter())
+                .registerTypeAdapter(MultilingualString.class, new MultilingualStringTypeAdapter())
+                .registerTypeAdapter(MunicipalityId.class, new DomainObjectIdJsonTypeAdapter<>(MunicipalityId::new))
+                .registerTypeAdapter(PhoneNumber.class, new PhoneNumberJsonTypeAdapter())
+                .registerTypeAdapter(ResourceId.class, new DomainObjectIdJsonTypeAdapter<>(ResourceId::new))
+                .registerTypeAdapter(ResourceStateId.class, new DomainObjectIdJsonTypeAdapter<>(ResourceStateId::new))
+                .registerTypeAdapter(ResourceTypeId.class, new DomainObjectIdJsonTypeAdapter<>(ResourceTypeId::new))
+                .registerTypeAdapter(StationId.class, new DomainObjectIdJsonTypeAdapter<>(StationId::new));
+
+    }
+}
--- JavaModules/idispatch-client/src/test/java/net/pkhapps/idispatch/client/v3/test/JsonObjectTest.java
+++ JavaModules/idispatch-client/src/test/java/net/pkhapps/idispatch/client/v3/test/JsonObjectTest.java
@@ -2,18 +2,7 @@ package net.pkhapps.idispatch.client.v3.test;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import net.pkhapps.idispatch.client.v3.AssignmentId;
-import net.pkhapps.idispatch.client.v3.base.gson.DomainObjectIdJsonTypeAdapter;
-import net.pkhapps.idispatch.client.v3.type.*;
-import net.pkhapps.idispatch.client.v3.util.Color;
-import net.pkhapps.idispatch.client.v3.util.MultilingualString;
-import net.pkhapps.idispatch.client.v3.util.PhoneNumber;
-import net.pkhapps.idispatch.client.v3.util.gson.ColorJsonTypeAdapter;
-import net.pkhapps.idispatch.client.v3.util.gson.InstantJsonTypeAdapter;
-import net.pkhapps.idispatch.client.v3.util.gson.MultilingualStringTypeAdapter;
-import net.pkhapps.idispatch.client.v3.util.gson.PhoneNumberJsonTypeAdapter;
-
-import java.time.Instant;
+import net.pkhapps.idispatch.client.v3.gson.GsonConfigurer;
 
 /**
  * Base class for objects that serialize to JSON using GSON.
@@ -24,19 +13,7 @@ public abstract class JsonObjectTest {
     private Gson gson;
 
     public JsonObjectTest() {
-        gson = new GsonBuilder()
-                .registerTypeAdapter(AssignmentId.class, new DomainObjectIdJsonTypeAdapter<>(AssignmentId::new))
-                .registerTypeAdapter(Color.class, new ColorJsonTypeAdapter())
-                .registerTypeAdapter(Instant.class, new InstantJsonTypeAdapter())
-                .registerTypeAdapter(MultilingualString.class, new MultilingualStringTypeAdapter())
-                .registerTypeAdapter(MunicipalityId.class, new DomainObjectIdJsonTypeAdapter<>(MunicipalityId::new))
-                .registerTypeAdapter(PhoneNumber.class, new PhoneNumberJsonTypeAdapter())
-                .registerTypeAdapter(ResourceId.class, new DomainObjectIdJsonTypeAdapter<>(ResourceId::new))
-                .registerTypeAdapter(ResourceStateId.class, new DomainObjectIdJsonTypeAdapter<>(ResourceStateId::new))
-                .registerTypeAdapter(ResourceTypeId.class, new DomainObjectIdJsonTypeAdapter<>(ResourceTypeId::new))
-                .registerTypeAdapter(StationId.class, new DomainObjectIdJsonTypeAdapter<>(StationId::new))
-                .setPrettyPrinting()
-                .create();
+        gson = new GsonConfigurer().configure(new GsonBuilder().setPrettyPrinting()).create();
     }
 
     protected Gson getGson() {
