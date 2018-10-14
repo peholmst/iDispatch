0aa8429b70cd4ac5465eb629e9ff48a4eb4f4dbb
Petter Holmström <petter.holmstrom@outlook.com>
2018-10-14 20:19:39 +0300

    Add JavaDocs to DomainEvent interfaces

--- JavaModules/idispatch-domain-support/src/main/java/net/pkhapps/idispatch/domain/support/AggregateDomainEvent.java
+++ JavaModules/idispatch-domain-support/src/main/java/net/pkhapps/idispatch/domain/support/AggregateDomainEvent.java
@@ -2,8 +2,16 @@ package net.pkhapps.idispatch.domain.support;
 
 import javax.annotation.Nonnull;
 
+/**
+ * Domain event interface for events published by an {@link AggregateRoot}.
+ *
+ * @param <ID> the ID type of the aggregate that published the event.
+ */
 public interface AggregateDomainEvent<ID extends DomainObjectId> extends DomainEvent {
 
+    /**
+     * Returns the ID of the aggregate root that published the event.
+     */
     @Nonnull
     ID publishedBy();
 }
--- JavaModules/idispatch-domain-support/src/main/java/net/pkhapps/idispatch/domain/support/DomainEvent.java
+++ JavaModules/idispatch-domain-support/src/main/java/net/pkhapps/idispatch/domain/support/DomainEvent.java
@@ -4,8 +4,20 @@ import javax.annotation.Nonnull;
 import java.io.Serializable;
 import java.time.Instant;
 
+/**
+ * Interface defining a domain event. A domain event is something that happens inside the domain model that other
+ * domains (or aggregates within the same domain) may be interested in.
+ *
+ * @see DomainEventPublisher
+ * @see DomainEventStore
+ * @see AggregateDomainEvent
+ * @see AggregateRoot#registerDomainEvent(DomainEvent)
+ */
 public interface DomainEvent extends Serializable {
 
+    /**
+     * Returns the date and time on which the domain event occurred.
+     */
     @Nonnull
     Instant occurredOn();
 }
