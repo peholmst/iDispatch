e9a755d4a5fdcf4964090b4a6f49cb6ab0b308bd
Petter Holmström <petter.holmstrom@outlook.com>
2019-03-27 23:04:52 +0200

    Add skeleton for GIS server

---
+++ Java/idispatch.gis.server/src/main/java/net/pkhapps/idispatch/gis/server/rest/PingController.java
@@ -0,0 +1,17 @@
+package net.pkhapps.idispatch.gis.server.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * TODO Document me!
+ */
+@RestController
+public class PingController {
+
+    @GetMapping(path = "/ping")
+    public ResponseEntity<Void> ping() {
+        return ResponseEntity.noContent().build();
+    }
+}
