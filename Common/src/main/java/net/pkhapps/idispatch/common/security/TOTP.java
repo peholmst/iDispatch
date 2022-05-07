8c4e3a304302adffe8c63107f29d49e9b21620a4
Petter Holmström <petter.holmstrom@outlook.com>
2022-05-07 11:29:28 +0300

    Initial commit of iDispatch Common

---
+++ Common/src/main/java/net/pkhapps/idispatch/common/security/TOTP.java
@@ -0,0 +1,259 @@
+/*
+ * iDispatch Common
+ *
+ * Copyright (c) 2022 Petter Holmström
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.pkhapps.idispatch.common.security;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.HexFormat;
+
+/**
+ * Implementation of the Time-based One-Time Password algorithm (RFC-6238) that always uses the {@value #HMAC_ALGORITHM}
+ * algorithm with a key length of {@value #HMAC_KEY_LENGTH} bytes, a {@code T0} value of 0 and an {@code X} value of 30
+ * seconds.
+ *
+ * @see #generateOneTimePassword(SharedSecret, Instant)
+ * @see #generateOneTimePassword(SharedSecret, BigInteger)
+ */
+public final class TOTP {
+
+    private static final String HMAC_ALGORITHM = "HmacSHA512";
+    private static final int HMAC_KEY_LENGTH = 64;
+    private static final BigInteger T0 = BigInteger.valueOf(0);
+    private static final BigInteger X = BigInteger.valueOf(30);
+
+    private TOTP() {
+    }
+
+    static byte[] leastSignificantBytesWithZeroPadding(byte[] source, int length) {
+        var result = new byte[length];
+        for (var i = 1; i <= result.length; ++i) {
+            result[result.length - i] = source[source.length - i];
+            if (source.length == i) {
+                break;
+            }
+        }
+        return result;
+    }
+
+    static byte[] hexStringToBytes(String hexString) {
+        if (hexString.length() % 2 == 1) {
+            hexString = "0" + hexString;
+        }
+        var length = hexString.length() / 2;
+        var bytes = new byte[length];
+        byte b;
+        for (var i = 0; i < hexString.length(); i += 2) {
+            b = (byte) (HexFormat.fromHexDigit(hexString.charAt(i)) << 4);
+            b += HexFormat.fromHexDigit(hexString.charAt(i + 1));
+            bytes[i / 2] = b;
+        }
+        return bytes;
+    }
+
+    /**
+     * Generates a new one-time password based on the given shared secret and timestamp.
+     *
+     * @param sharedSecret the shared secret.
+     * @param timestamp    the timestamp.
+     * @return the one-time password.
+     */
+    public static OneTimePassword generateOneTimePassword(SharedSecret sharedSecret, Instant timestamp) {
+        return generateOneTimePassword(sharedSecret, calculateTimeStep(timestamp));
+    }
+
+    /**
+     * Generates a new one-time password based on the given shared secret and time step.
+     *
+     * @param sharedSecret the shared secret.
+     * @param timeStep     the time step.
+     * @return the one-time password.
+     * @throws RuntimeException if the one-time password could not be generated (should never happen if the JVM has the
+     *                          correct algorithm installed).
+     */
+    public static OneTimePassword generateOneTimePassword(SharedSecret sharedSecret, BigInteger timeStep) {
+        try {
+            var mac = Mac.getInstance(HMAC_ALGORITHM);
+            sharedSecret.initMac(mac, HMAC_KEY_LENGTH);
+            var msg = leastSignificantBytesWithZeroPadding(timeStep.toByteArray(), 8);
+            var result = mac.doFinal(msg);
+            return new OneTimePassword(result);
+        } catch (NoSuchAlgorithmException | InvalidKeyException ex) {
+            throw new RuntimeException("Error generating one time password", ex);
+        }
+    }
+
+    /**
+     * Calculates the time step for the given timestamp.
+     *
+     * @param timestamp the timestamp to calculate the time step for.
+     * @return the time step.
+     */
+    public static BigInteger calculateTimeStep(Instant timestamp) {
+        return calculateTimeStep(BigInteger.valueOf(timestamp.getEpochSecond()));
+    }
+
+    /**
+     * Calculates the time step for the given epoch seconds.
+     *
+     * @param epochSeconds the epoch seconds to calculate the time step for.
+     * @return the time step.
+     */
+    public static BigInteger calculateTimeStep(BigInteger epochSeconds) {
+        return epochSeconds.subtract(T0).divide(X);
+    }
+
+    /**
+     * Class representing a shared secret used to calculate a one-time password.
+     */
+    @SuppressWarnings("ClassCanBeRecord")
+    public static final class SharedSecret {
+
+        private final byte[] data;
+
+        private SharedSecret(byte[] data) {
+            this.data = data;
+        }
+
+        /**
+         * Creates a new shared secret from the given hexadecimal string.
+         *
+         * @param hexString the hexadecimal representation of the shared secret.
+         * @return the shared secret.
+         */
+        public static SharedSecret fromHexString(String hexString) {
+            return new SharedSecret(hexStringToBytes(hexString));
+        }
+
+        /**
+         * Creates a new shared secret from the given Base64 encoded string.
+         *
+         * @param base64String a Base64 representation of the shared secret.
+         * @return the shared secret.
+         */
+        public static SharedSecret fromBase64String(String base64String) {
+            return new SharedSecret(Base64.getDecoder().decode(base64String));
+        }
+
+        /**
+         * Creates a new, random shared secret. Please note that this secret must be shared with the other party in
+         * order to be useful. A cryptographically strong algorithm will be used to generate the secret.
+         *
+         * @return the shared secret.
+         * @throws RuntimeException if the shared secret could not be generated (should never happen if the JVM has the
+         *                          correct algorithm installed)
+         * @see #toHexString()
+         * @see #toBase64String()
+         */
+        public static SharedSecret fromRandom() {
+            try {
+                var random = SecureRandom.getInstanceStrong();
+                var bytes = new byte[HMAC_KEY_LENGTH];
+                random.nextBytes(bytes);
+                return new SharedSecret(bytes);
+            } catch (NoSuchAlgorithmException ex) {
+                throw new RuntimeException("Error generating shared secret", ex);
+            }
+        }
+
+        /**
+         * Returns the shared secret as a hexadecimal string.
+         *
+         * @return a hexadecimal string.
+         * @see #fromHexString(String)
+         */
+        public String toHexString() {
+            var sb = new StringBuilder();
+            for (byte b : data) {
+                sb.append(Integer.toHexString(b & 0xff));
+            }
+            var len = data.length * 2;
+            while (sb.length() < len) {
+                sb.insert(0, "0");
+            }
+            return sb.toString();
+        }
+
+        /**
+         * Returns the shared secret as a Base64 encoded string.
+         *
+         * @return a Base64 encoded string.
+         * @see #fromBase64String(String)
+         */
+        public String toBase64String() {
+            return Base64.getEncoder().encodeToString(data);
+        }
+
+        private void initMac(Mac mac, int keyLength) throws InvalidKeyException {
+            if (keyLength == data.length) {
+                mac.init(new SecretKeySpec(data, "RAW"));
+            } else {
+                byte[] key = new byte[keyLength];
+                for (var i = 0; i < keyLength; ++i) {
+                    key[i] = data[i % data.length];
+                }
+                mac.init(new SecretKeySpec(key, "RAW"));
+            }
+        }
+    }
+
+    /**
+     * Class representing a one-time password.
+     *
+     * @see #truncate(int)
+     */
+    @SuppressWarnings("ClassCanBeRecord")
+    public static final class OneTimePassword {
+
+        private static final int[] DIGITS_POWER = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
+        private final byte[] data;
+
+        private OneTimePassword(byte[] data) {
+            this.data = data;
+        }
+
+        /**
+         * Truncates the password into a numerical string with the given number of digits.
+         *
+         * @param digits a number between 0 (not useful) and 8 (inclusive).
+         * @return the truncated password.
+         * @throws IllegalArgumentException if the number of digits is not within the given range.
+         */
+        public String truncate(int digits) {
+            if (digits > 8) {
+                throw new IllegalArgumentException("Number of digits must not be greater than 8");
+            }
+            var offset = data[data.length - 1] & 0xf;
+            var binCode = (data[offset] & 0x7f) << 24 | (data[offset + 1] & 0xff) << 16 | (data[offset + 2] & 0xff) << 8 | (data[offset + 3] & 0xff);
+            var otp = binCode % DIGITS_POWER[digits];
+            var sb = new StringBuilder(Integer.toString(otp));
+            while (sb.length() < digits) {
+                sb.insert(0, "0");
+            }
+            return sb.toString();
+        }
+
+    }
+}
---
+++ Common/src/test/java/net/pkhapps/idispatch/common/security/TOTPTest.java
@@ -0,0 +1,103 @@
+/*
+ * iDispatch Common
+ *
+ * Copyright (c) 2022 Petter Holmström
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.pkhapps.idispatch.common.security;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TOTPTest {
+
+    private static final TOTP.SharedSecret SECRET = TOTP.SharedSecret.fromHexString("31323334353637383930");
+
+    @Test
+    public void leastSignificantBytesWithZeroPadding_smallerSource() {
+        assertArrayEquals(new byte[]{0, 0, 0, (byte) 0xff}, TOTP.leastSignificantBytesWithZeroPadding(new byte[]{(byte) 0xff}, 4));
+    }
+
+    @Test
+    public void leastSignificantBytesWithZeroPadding_equalSource() {
+        assertArrayEquals(new byte[]{0x12, 0x23, 0x34, 0x45}, TOTP.leastSignificantBytesWithZeroPadding(new byte[]{0x12, 0x23, 0x34, 0x45}, 4));
+    }
+
+    @Test
+    public void leastSignificantBytesWithZeroPadding_largerSource() {
+        assertArrayEquals(new byte[]{0x12, 0x23, 0x34, 0x45}, TOTP.leastSignificantBytesWithZeroPadding(new byte[]{0x01, 0x12, 0x23, 0x34, 0x45}, 4));
+    }
+
+    @Test
+    public void hexStringToBytes_evenNumberOfChars() {
+        assertArrayEquals(new byte[]{0x12, 0x23, 0x34, 0x45}, TOTP.hexStringToBytes("12233445"));
+    }
+
+    @Test
+    public void hexStringToBytes_oddNumberOfChars() {
+        assertArrayEquals(new byte[]{0x02, 0x23, 0x34, 0x45}, TOTP.hexStringToBytes("2233445"));
+    }
+
+    @Test
+    public void generateOneTimePassword_rfc6238TestVectors() {
+        assertEquals("90693936", TOTP.generateOneTimePassword(SECRET, Instant.ofEpochSecond(59)).truncate(8));
+        assertEquals("25091201", TOTP.generateOneTimePassword(SECRET, Instant.ofEpochSecond(1111111109)).truncate(8));
+        assertEquals("99943326", TOTP.generateOneTimePassword(SECRET, Instant.ofEpochSecond(1111111111)).truncate(8));
+        assertEquals("93441116", TOTP.generateOneTimePassword(SECRET, Instant.ofEpochSecond(1234567890)).truncate(8));
+        assertEquals("38618901", TOTP.generateOneTimePassword(SECRET, Instant.ofEpochSecond(2000000000)).truncate(8));
+        assertEquals("47863826", TOTP.generateOneTimePassword(SECRET, TOTP.calculateTimeStep(new BigInteger("20000000000"))).truncate(8));
+    }
+
+    @Test
+    public void oneTimePasswordTruncate_differentLengths() {
+        var otp = TOTP.generateOneTimePassword(SECRET, Instant.ofEpochSecond(2000000000));
+        assertEquals("38618901", otp.truncate(8));
+        assertEquals("8618901", otp.truncate(7));
+        assertEquals("618901", otp.truncate(6));
+        assertEquals("18901", otp.truncate(5));
+        assertEquals("8901", otp.truncate(4));
+        assertEquals("901", otp.truncate(3));
+        assertEquals("01", otp.truncate(2));
+        assertEquals("1", otp.truncate(1));
+        assertEquals("0", otp.truncate(0));
+    }
+
+    @Test
+    public void sharedSecretFromRandom_noExceptionThrown() {
+        var secret = TOTP.SharedSecret.fromRandom();
+        // No assertions, just shouldn't throw any exceptions
+        assertEquals(128, secret.toHexString().length());
+    }
+
+    @Test
+    public void sharedSecretToHexAndBack() {
+        var hex = "00112233445566778899aabbccddeeff";
+        var secret = TOTP.SharedSecret.fromHexString(hex);
+        assertEquals(hex, secret.toHexString());
+    }
+
+    @Test
+    public void sharedSecretToBase64AndBack() {
+        var base64 = "YWJjZGVmZwo=";
+        var secret = TOTP.SharedSecret.fromBase64String(base64);
+        assertEquals(base64, secret.toBase64String());
+    }
+}
